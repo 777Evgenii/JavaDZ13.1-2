bf0df7cf9ad908035cbfdcfaba7943da96a93d66
Evgenii <mikhailovich21@mail.ru>
2023-07-05 15:13:01 +0500

    Добавление тестов

--- src/test/java/ru/netology/manager/ProductManagerTest.java
+++ src/test/java/ru/netology/manager/ProductManagerTest.java
@@ -14,14 +14,31 @@ public class ProductManagerTest {
     Book book1 = new Book(3, "Book1", 30, "John Doe");
     Smartphone smartphone1 = new Smartphone(4, "SmartPhone1", 40, "Xiaomi");
 
-    @Test
-    public void shouldFindSecondItems() {
-        ProductManager productManager = new ProductManager(new ProductRepository());
+    private final ProductManager productManager = new ProductManager(new ProductRepository());
+
+    public ProductManagerTest() {
         productManager.add(product1);
         productManager.add(product2);
         productManager.add(book1);
         productManager.add(smartphone1);
+    }
 
+    @Test
+    public void shouldFindFindNothing() {
+        Product[] expected = new Product[]{};
+        Product[] actual = productManager.searchBy("ProductNothing");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindOneProduct() {
+        Product[] expected = new Product[]{book1};
+        Product[] actual = productManager.searchBy("Book1");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindTwoProducts() {
         Product[] expected = new Product[]{product1, product2};
         Product[] actual = productManager.searchBy("Product");
         assertArrayEquals(expected, actual);
